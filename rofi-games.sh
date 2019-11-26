2eaf0b6b50a84640ec4e6942f1fdfb6c8d038430
TWB0109 <burandonsp@gmail.com>
2019-11-26 01:35:39 -0600

    Added New games to games menu

--- rofi-games.sh
+++ rofi-games.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 varName=$(echo "DDLC
-OffTheBalls" | rofi -dmenu -p "Games")
+OffTheBalls
+Enter the Gungeon
+Party Hard
+SteamWorld Dig
+Undertale" | rofi -dmenu -p "Games")
 
 if [ "$varName" == "DDLC" ];
 then
@@ -10,4 +14,24 @@ fi
 if [ "$varName" == "OffTheBalls" ];
 then
   cd ~/Games/OffTheBalls/ && ./OffTheBalls.x86_64 &
+fi
+
+if [ "$varName" == "Enter the Gungeon" ];
+then
+  cd ~/Games/Enter\ The\ Gungeon/ && ./EtG.x86_64 &
+fi
+
+if [ "$varName" == "Party Hard" ];
+then
+  cd ~/Games/Party\ Hard/ && ./PartyHardGame.x86_64 &
+fi
+
+if [ "$varName" == "SteamWorld Dig" ];
+then
+  cd ~/Games/SteamWorld\ Dig/ && cd game && ./SteamWorldDig &
+fi
+
+if [ "$varName" == "Undertale" ];
+then
+  cd ~/Games/Undertale/ && cd game && ./UNDERTALE &
 fi
