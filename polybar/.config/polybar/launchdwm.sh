b342185b06c47c703920ef88a47a6c651a5f908e
TWB0109 <burandonsp@gmail.com>
2020-11-01 16:32:16 -0600

    Multimonitor support

--- polybar/.config/polybar/launchdwm.sh
+++ polybar/.config/polybar/launchdwm.sh
@@ -6,7 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-/usr/local/bin/polybar example &
+# Launch example 
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m /usr/local/bin/polybar --reload example &
+    done
+else 
+    /usr/local/bin/polybar --reload example &
+fi
 
 echo "Bars launched..."
