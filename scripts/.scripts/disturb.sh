050c0870c7c1e427b9e4165bdc70051421dbfa98
TWB0109 <53587182+TWB0109@users.noreply.github.com>
2021-07-08 23:00:07 -0600

    pausing dunst is very easy

--- scripts/.scripts/disturb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-notify-send "Disturbing"
-notify-send "DUNST_COMMAND_RESUME"
-
-exit
--- scripts/.scripts/donotdisturb.sh
+++ scripts/.scripts/donotdisturb.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-notify-send "Not Disturbing"
-sleep 5
-notify-send "DUNST_COMMAND_PAUSE"
-
-exit
+dunstctl set-paused toggle
