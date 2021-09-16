f0955fc57d36bf9f07387c5077aea049983c2467
TWB0109 <53587182+TWB0109@users.noreply.github.com>
2021-09-15 21:35:23 -0600

    meh

--- spectrwm/.config/spectrwm/autostart.sh
+++ spectrwm/.config/spectrwm/autostart.sh
@@ -5,5 +5,4 @@ setxkbmap -option caps:swapescape\
 nitrogen --restore
 xrdb ~/.Xresources
 $HOME/.scripts/natscroll
-xss-lock --transfer-sleep-lock -- "betterlockscreen -l dimblur" --nofork &
 mpd &
--- spectrwm/.config/spectrwm/baraction.sh
+++ spectrwm/.config/spectrwm/baraction.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 vol(){
     vol=$(amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "MM" } else { print $2 }}' | head -n 1)
-    echo -e "+@fg=1;+@fn=2;墳+@fn=0; $vol"
+    echo -e "+@fg=1;+@fn=2;[ 墳+@fn=0;  $vol ]"
 }
 
 wifi(){
     wifi=$(nmcli | sed '1q' | awk '{print $4}')
-    [[ -n $wifi ]] && echo -e "+@fg=4;+@fn=2;直+@fn=0; $wifi" || echo -e "+@fg=1;+@fn=2;直+@fn=0; DISCONNECTED"
+    [[ -n $wifi ]] && echo -e "+@fg=4;+@fn=2;[ 直+@fn=0;  $wifi ]" || echo -e "+@fg=1;+@fn=2;[ 直+@fn=0;  DISCONNECTED ]"
 }
 
 bri(){
     bri=$(light | cut -d. -f1)
-    echo -e "+@fg=3;+@fn=2;+@fn=0; $bri"
+    echo -e "+@fg=3;+@fn=2;[+@fn=0;  $bri ]"
 }
 
 I(){
@@ -20,6 +20,6 @@ I(){
 
 SLEEP_SEC=1
 while true; do 
-    echo "$(vol)$(I)$(wifi)$(I)$(bri) "
+    echo "$(vol) $(wifi) "
     sleep $SLEEP_SEC
 done
