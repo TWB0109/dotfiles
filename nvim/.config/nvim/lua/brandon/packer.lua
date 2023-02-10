0447eac68f6b693488348d3f8bda8ade70a0fcb3
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-02-10 13:25:57 -0600

    Gitsigns is pretty

--- nvim/.config/nvim/lua/brandon/packer.lua
+++ nvim/.config/nvim/lua/brandon/packer.lua
@@ -88,4 +88,10 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+        end
+    }
 end)
