1f505332b91e6d8f0c31ab3701903cc8259a6c0a
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-02-26 15:42:51 -0600

    Added a binding to list buffers with telescope

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Open a fuzzy file picker"})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Open a fuzzy buffer picker"})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = "Open a git repo fuzzy file picker"})
 vim.keymap.set('n', '<leader>lg', builtin.live_grep, {desc = "Open a fuzzy live grep"})
