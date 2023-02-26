3daed99dbad24ab21c4a7f5d1d54c5f7d14850e2
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-02-26 16:01:42 -0600

    handy function to set keymaps for terminal buffers from akinsho/toggleterm.nvim

--- nvim/.config/nvim/lua/brandon/remap.lua
+++ nvim/.config/nvim/lua/brandon/remap.lua
@@ -2,7 +2,19 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = '  '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, {desc = "Opens Netrw"})
 
-vim.keymap.set('t', '<ESC>', '<C-\\><C-n>')
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 
 vim.keymap.set('n', '<Up>', ':resize +2<CR>')
 vim.keymap.set('n', '<Down>', ':resize -2<CR>')
