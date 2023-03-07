273b74aecf6eda458b5dbf6bc97ab509fbe9af06
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-03-07 02:02:58 -0600

    Installed neodev.nvim

--- nvim/.config/nvim/lua/brandon/packer.lua
+++ nvim/.config/nvim/lua/brandon/packer.lua
@@ -148,4 +148,6 @@ return require('packer').startup(function(use)
     use {"akinsho/toggleterm.nvim", tag = '*', config = function()
         require("toggleterm").setup()
     end}
+
+    use "folke/neodev.nvim"
 end)
