6c9d149e702d54c3676eac55161d45f480032a1f
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-03-30 16:30:22 -0600

    Made fugitive lazy loaded, not needed unless doing git stuff

--- nvim/.config/nvim/lua/brandon/lazy.lua
+++ nvim/.config/nvim/lua/brandon/lazy.lua
@@ -61,7 +61,12 @@ return require('lazy').setup({
         config = function()
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git, {desc = "Open fuGITive status"})
             vim.keymap.set("n", "<leader>gp", '<CMD>Git push<CR>', {desc = "Push commits"})
-        end
+        end,
+        cmd = 'Git',
+        keys = {
+            { '<leader>gs', vim.cmd.Git, desc = 'Open fuGITive status' },
+            { '<leader>gs', '<CMD>Git push<CR>', desc = "Push commits"}
+        }
     },
 
     {
