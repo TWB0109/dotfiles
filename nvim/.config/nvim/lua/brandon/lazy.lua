ee882c12616aca61825dd5977e50de13e82bc1f7
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-04-08 14:25:30 -0600

    installed firenvim

--- nvim/.config/nvim/lua/brandon/lazy.lua
+++ nvim/.config/nvim/lua/brandon/lazy.lua
@@ -489,5 +489,17 @@ return require('lazy').setup({
             ]])
             vim.g.vimwiki_global_ext = 0
         end
+    },
+
+    {
+        'glacambre/firenvim',
+
+        -- Lazy load firenvim
+        -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+        cond = not not vim.g.started_by_firenvim,
+        build = function()
+            require("lazy").load({ plugins = "firenvim", wait = true })
+            vim.fn["firenvim#install"](0)
+        end
     }
 })
