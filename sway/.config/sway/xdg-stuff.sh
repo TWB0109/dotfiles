852598c8036d02850229e5835e64c7b10175fe24
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-09-06 22:52:28 -0600

    a

---
+++ sway/.config/sway/xdg-stuff.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-hyprland
+systemctl --user start wireplumber pipewire xdg-desktop-portal xdg-desktop-portal-hyprland
