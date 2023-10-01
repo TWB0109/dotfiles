f9c0b44a7bebcd9df08262b5482ef8edf9786ee3
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-10-01 15:24:23 -0600

    goddamnit, xdg-desktop-portal config has gotten very complex

--- hypr/.config/hypr/xdg-stuff.sh
+++ hypr/.config/hypr/xdg-stuff.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=hyprland
-systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-hyprland
-systemctl --user start wireplumber pipewire xdg-desktop-portal xdg-desktop-portal-hyprland
+systemctl --user stop pipewire 
+systemctl --user stop wireplumber 
+systemctl --user stop xdg-desktop-portal 
+systemctl --user stop xdg-desktop-portal-hyprland
+systemctl --user start pipewire 
+systemctl --user start wireplumber 
+systemctl --user start xdg-desktop-portal 
+systemctl --user start xdg-desktop-portal-hyprland
