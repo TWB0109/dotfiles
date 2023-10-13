f247a126d2ed0af21f1fb75679a03e782d0dc7bb
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-10-13 00:47:16 -0600

    colors

--- nwg-wrapper/.config/nwg-wrapper/date.sh
+++ nwg-wrapper/.config/nwg-wrapper/date.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 time=$(LC_ALL=C TZ='America/Costa_Rica' date +' %a, %b %d, %Y | %H:%M')
-echo '<span size="20000" foreground="#79740e" background="#f2e5bc">'$time'</span>'
+echo '<span size="20000" foreground="#b8bb26" background="#282828">'$time'</span>'
