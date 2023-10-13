8b6ac5cf6b39ce01d9541fea8e5b585ff9820fce
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-10-13 00:46:30 -0600

    gg=G is the proper command to indent the whole file

--- nvim/.config/nvim/lua/brandon/remap.lua
+++ nvim/.config/nvim/lua/brandon/remap.lua
@@ -29,5 +29,5 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
 
-vim.keymap.set("n", "<leader>=", "=G", {desc = "Properly indents the whole file"})
+vim.keymap.set("n", "<leader>=", "gg=G", {desc = "Properly indents the whole file"})
 vim.keymap.set("i", "<C-BS>", "<C-W>", {desc = "Ciw"})
