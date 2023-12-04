247b897c1af92fc11bb6ffb574ec380e157b7e04
Brandon <53587182+TWB0109@users.noreply.github.com>
2023-12-03 21:29:46 -0600

    buncha shit

--- nvim/.config/nvim/lua/brandon/lazy.lua
+++ nvim/.config/nvim/lua/brandon/lazy.lua
@@ -284,11 +284,6 @@ return require('lazy').setup({
                         'encoding',
                         'fileformat',
                         'filetype',
-                        {
-                            require("noice").api.statusline.mode.get,
-                            cond = require("noice").api.statusline.mode.has,
-                            color = { fg = "red" }
-                        }
                     },
                     lualine_y = { 'progress' },
                     lualine_z = { 'location' }
