ee1a8bde4822ae63327280f1a39645085fbaf6de
Brandon <burandonsp@gmail.com>
2024-01-31 01:40:19 -0600

    some spring cleaning

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,5 @@
 {
   description = "NixOS config";
-
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
@@ -12,31 +11,29 @@
   };
 
   outputs = { nixpkgs, home-manager, nixvim, ... }: 
+
   let
     system = "x86_64-linux";
-
     pkgs = import nixpkgs {
       inherit system;
       config = { allowUnfree = true; };
     };
-
     lib = nixpkgs.lib;
 
   in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
         inherit system;
-
-	modules = [
-	  ./nixos/configuration.nix
-	  home-manager.nixosModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.brandon = import ./home-manager/home.nix;
-	  }
-	  nixvim.nixosModules.nixvim
-	];
+	      modules = [
+	        ./nixos/configuration.nix
+	        home-manager.nixosModules.home-manager
+	        {
+	          home-manager.useGlobalPkgs = true;
+	          home-manager.useUserPackages = true;
+	          home-manager.users.brandon = import ./home-manager/home.nix;
+	        }
+	        nixvim.nixosModules.nixvim
+	      ];
       };
     };
   };
