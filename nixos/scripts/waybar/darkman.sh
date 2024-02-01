d82119ff8c97a348017ba346b4c468aa089948b2
Brandon <burandonsp@gmail.com>
2024-02-01 02:13:29 -0600

    Changed shebangs to be nix compatible

--- nixos/scripts/waybar/darkman.sh
+++ nixos/scripts/waybar/darkman.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p bash mako
 
 state=$(darkman get)
 
--- nixos/scripts/waybar/ddc.sh
+++ nixos/scripts/waybar/ddc.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p bash mako
 
 state=$(ddcutil --noverify --sleep-multiplier .1 getvcp 10 | grep -o -P '\d*,' | grep -Eo '[0-9]' | tr -d '\n')
 echo $state
