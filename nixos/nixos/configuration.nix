6f275ea63de37532a04e3c79400a36bdc6e0ee60
Brandon <burandonsp@gmail.com>
2024-02-12 23:52:26 -0600

    I feel dumb

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -44,8 +44,10 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = false;
   services.xserver.desktopManager.gnome.enable = false;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
   
   # Configure keymap in X11
   services.xserver = {
@@ -99,7 +101,7 @@
   };
 
   # Enable Hyprland
-  programs.hyprland.enable = true;
+  programs.hyprland.enable = false;
   programs.dconf.enable = true;
 
   # Hint Electron apps to use wayland:
